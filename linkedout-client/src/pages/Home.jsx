5614d1d7f4eee08fac736cef076b7e0f53020a96
oktclm <cilmio@tcd.ie>
2024-07-29 12:12:31 +0300

    search bar and location search bar added

--- linkedout-client/src/pages/Home.jsx
+++ linkedout-client/src/pages/Home.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {useState} from 'react';
+import Banner from '../components/Banner'
 
 const Home = () => {
+    const [query, setQuery] = useState("");
+    const handleInputChange = (event) => {
+      setQuery(event.target.value)
+    }
   return (
-    <div>HomePgae</div>
+    <div>
+      <Banner query={query} handleInputChange={handleInputChange}/>
+    </div>
   )
 }
 
