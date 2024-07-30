d7e488e71d9f31af096849c5f0b505178a6d9cc9
oktclm <cilmio@tcd.ie>
2024-07-30 18:07:20 +0300

    added left side bar, right side bar and pagination

--- linkedout-client/src/pages/Home.jsx
+++ linkedout-client/src/pages/Home.jsx
@@ -2,14 +2,21 @@ import {useEffect, useState} from 'react';
 import Banner from '../components/Banner'
 import Card from '../components/Card';
 import Jobs from './Jobs';
+import Sidebar from '../sidebar/Sidebar';
+import Newsletter from '../sidebar/Newsletter';
 
 const Home = () => {
     const [selectedCategory, setSelectedCategory] = useState(null);
     const[jobs, setJobs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 6;
 
     useEffect(() => {
+      setIsLoading(true);
       fetch("jobs.json").then(res => res.json()).then(data => {
         setJobs(data)
+        setIsLoading(false)
       })
     }, [])
 
@@ -28,6 +35,24 @@ const Home = () => {
       setSelectedCategory(event.target.value)
     }
 
+    const calculatePageRange = () => {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      return {startIndex, endIndex};
+    }
+
+    const nextPage = () => {
+      if(currentPage < Math.ceil(filteredItems.length / itemsPerPage)) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+
+    const prevPage = () => {
+      if(currentPage > 1) {
+        setCurrentPage(currentPage - 1)
+      }
+    }
+
     const filteredData = (jobs, selected, query) => {
       let filteredJobs = jobs;
 
@@ -36,27 +61,55 @@ const Home = () => {
       }
 
       if(selected){
-        filteredData = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => (
+        console.log((selected));
+        filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => (
           jobLocation.toLowerCase() === selected.toLowerCase() ||
           parseInt(maxPrice) <= parseInt(selected) ||
+          postingDate >= selected ||
           salaryType.toLowerCase() === selected.toLowerCase() ||
+          experienceLevel.toLowerCase() === selected.toLowerCase() ||
           employmentType.toLowerCase() === selected.toLowerCase()
         ));
         console.log(filteredJobs);
       }
+
+      const {startIndex, endIndex} = calculatePageRange();
+      filteredJobs = filteredJobs.slice(startIndex, endIndex)
       return filteredJobs.map((data, i) => <Card key={i} data={data}/>)
     }
 
     const result = filteredData(jobs, selectedCategory, query);
+
   return (
     <div>
       <Banner query={query} handleInputChange={handleInputChange}/>
 
       {/* main content */}
       <div className='bg-[#FAFAFA] md:grid grid-cols-4 gap-8 lg:pg-24 px-4 py-12'>
-        <div className='bg-white p-4 rounded'>Left</div>
-        <div className='col-span-2 bg-white p-4 rounded-sm'> <Jobs result={result}/></div>
-        <div className='bg-white p-4 rounded'>Right</div>
+        <div className='bg-white p-4 rounded'>
+          <Sidebar handleChange={handleChange} handleClick={handleClick}/>
+        </div>
+
+        <div className='col-span-2 bg-white p-4 rounded-sm'>
+          {
+            isLoading ? (<p className='font-medium'>Loading....</p>) : result.length > 0 ? (<Jobs result={result}/>) : <>
+            <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
+            <p>No data found</p>
+            </>
+          }
+
+          {
+            result.length > 0 ? (
+              <div className='flex justify-center mt-4 space-x-8'>
+                <button onClick={prevPage} disabled={currentPage === 1}
+                className='hover:inderline'>Previous</button>
+                <span className='mx-2'>Page {currentPage} of {Math.ceil(filteredItems.length / itemsPerPage)}</span>
+                <button onClick={nextPage} disabled={currentPage === Math.ceil(filteredItems.length / itemsPerPage)} className='hover:underline'>Next</button>
+              </div>
+            ) : ""
+          }
+        </div>
+        <div className='bg-white p-4 rounded'><Newsletter/></div>
       </div>
     </div>
   )
